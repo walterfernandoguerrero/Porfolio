9547621882923f3a12676bc78fd0f7766dda6557
walter <guerrerowalterfernado@gmail.com>
2022-03-20 23:07:25 -0300

    primer commit

---
+++ funciones.js
@@ -0,0 +1,8 @@
+function  walter(){
+    let texto;
+    texto=" Hola!!! Soy Walter te cuento que vivo  en Garín  con mi esposa Carla tenemos dos hijos Rosario y Mateo, me gusta jugar al pádel, el futbol de arquero, la natación. Soy de Boca Jr. me gusta la música Nahuel Pennissi y Rali Barrionuevo son de mis favoritos entre otros.  "
+    alert(texto);
+}
+function perfil(){
+    alert("¡¡¡¡¡¡¡VAMOS A MI PERFIL DE PROGRAMACIóN¡¡¡¡¡¡¡");
+}
